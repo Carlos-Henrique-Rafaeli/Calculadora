1f4d304e4d5344caf29f671b4a40128ed551ade9
Carlos Henrique Rafaeli <gordinez658@gmail.com>
2025-03-09 12:39:46 -0300

    Operações de multiplicação + divisão e looping de código

--- Calculadora.ConsoleApp/Program.cs
+++ Calculadora.ConsoleApp/Program.cs
@@ -4,47 +4,104 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("--------------------------");
-            Console.WriteLine("Calculadora Tabajara 2025");
-            Console.WriteLine("--------------------------");
-            Console.WriteLine("");
 
-            Console.WriteLine("1 - Somar");
-            Console.WriteLine("2 - Subtrair");
-            Console.WriteLine("S - Sair");
+            bool calculadora = true;
 
-            Console.WriteLine("");
-            Console.Write("Escolha uma opção: ");
-
-            string opcao = Console.ReadLine().ToUpper();
-
-            if (opcao == "S")
+            while (calculadora)
             {
-                return;
+                Console.Clear();
+                
+                Console.WriteLine("--------------------------");
+                Console.WriteLine("Calculadora Tabajara 2025");
+                Console.WriteLine("--------------------------");
+                Console.WriteLine();
+
+                Console.WriteLine("1 - Somar");
+                Console.WriteLine("2 - Subtrair");
+                Console.WriteLine("3 - Multiplicar");
+                Console.WriteLine("4 - Dividir");
+                Console.WriteLine("S - Sair");
+
+                Console.WriteLine();
+                Console.Write("Escolha uma opção: ");
+
+                string opcao = Console.ReadLine().ToUpper();
+
+                if (opcao == "S")
+                {
+                    calculadora = false;
+                    break;
+                }
+                else if (opcao != "1" && opcao != "2" && opcao != "3" && opcao != "4") 
+                {
+                    Console.WriteLine("Opcão Inválida!");
+                    Console.ReadLine();
+                    continue;
+                }
+
+
+                Console.Write("Digite o Primeiro Número: ");
+                string primeiroNumeroString = Console.ReadLine();
+                int primeiroNumero = Convert.ToInt32(primeiroNumeroString);
+
+                Console.Write("Digite o Segundo Número: ");
+                string segundoNumeroString = Console.ReadLine();
+                int segundoNumero = Convert.ToInt32(segundoNumeroString);
+
+
+                int resultado = 0;
+                bool operacaoValida = true;
+
+
+                if (opcao == "1")
+                {
+                    resultado = primeiroNumero + segundoNumero;
+                }
+
+                else if (opcao == "2")
+                {
+                    resultado = primeiroNumero - segundoNumero;
+                }
+
+                else if (opcao == "3")
+                {
+                    resultado = primeiroNumero * segundoNumero;
+                }
+
+                else if (opcao == "4")
+                {
+
+                    if (segundoNumero != 0)
+                    {
+                        resultado = primeiroNumero / segundoNumero;
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("Divisão por 0!");
+                        operacaoValida = false;
+                    }
+                }
+
+
+                if (operacaoValida) 
+                {
+                    Console.WriteLine("Resultado: " + resultado);
+                }
+
+                
+                Console.Write("Deseja fazer outro cálculo? (s/n): ");
+                
+                string continuar = Console.ReadLine().ToLower();
+
+                if (continuar != "s")
+                {
+                    calculadora = false;
+                }
+
             }
             
-            Console.Write("Digite o Primeiro Número: ");
-            string primeiroNumeroString = Console.ReadLine();
-
-            int primeiroNumero = Convert.ToInt32(primeiroNumeroString);
-
-            Console.Write("Digite o Segundo Número: ");
-            string segundoNumeroString = Console.ReadLine();
-            int segundoNumero = Convert.ToInt32(segundoNumeroString);
-
-            int resultado = 0;
-
-
-            if (opcao == "1") {
-                resultado = primeiroNumero + segundoNumero;
-            }
-            else {
-                resultado = primeiroNumero - segundoNumero;
-            }
-
-
-            Console.WriteLine("Resultado: " + resultado);
-
+            Console.WriteLine("Calculadora Encerrada.");
             Console.ReadLine();
         }
     }
